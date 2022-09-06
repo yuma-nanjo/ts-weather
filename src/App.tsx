abe74a18b77b8458d1b3704b000d8c967d8c4ed9
yuma-nanjo <y.nanjo1110@gmail.com>
2022-09-06 18:04:58 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import Title from './components/Title';
+import Form from './components/Form';
+import Results from './components/Results';
 import './App.css';
+import React, { useState } from "react"
+type ResultsStateType = {
+  country: string;
+  cityName: string;
+  temperature: string;
+  conditionText: string;
+  icon: string;
+}
 
 function App() {
+  const [city, setCity] = useState<string>("");
+  const [results, setResults] = useState<ResultsStateType>({
+    country: "",
+    cityName: "",
+    temperature: "",
+    conditionText: "",
+    icon: ""
+  });
+  const getWeather = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    fetch(`https://api.weatherapi.com/v1/current.json?key=e25dc153302645ef84781433220609&q=${city}&aqi=no`).then(res => res.json()).then(data => {
+      setResults({
+        country: data.location.country,
+        cityName: data.location.name,
+        temperature: data.current.temp_c,
+        conditionText: data.current.condition.text,
+        icon: data.current.condition.icon,
+      })
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="container">
+        <Title />
+        <Form setCity={setCity} getWeather={getWeather} />
+        <Results results={results} />
+      </div>
     </div>
   );
 }
